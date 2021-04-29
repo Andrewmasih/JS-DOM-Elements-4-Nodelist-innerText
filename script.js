04e031e8bc86aa349b258d597aa0b0b2dfa0bbe4
Andrew Masih <armasih@outlook.com>
2021-04-29 14:59:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let getFit = document.getElementsByTagName ("h1");
+console.log (getFit [0].innerText);
+
+let weeklyRoutine = document.getElementsByClassName ("exercise-routine");
+  for (routine of weeklyRoutine) {
+    console.log (routine.innerText)
+  };
+
+  /* the 'nodes' in the ''Nodelist' can be accessed by index numbers. the index starts from 0 just like in a array. We can also iterate through the collection that these methods returns in the same way we would iterate through an array. */
+
